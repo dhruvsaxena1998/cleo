09e6e4fe5341f786093af0a454ae1bf22551dbf1
dhruvsaxena1998 <dhruvsaxena119@gmail.com>
2026-05-07 19:23:38 +0530

    feat(tui): state polling and pane capture commands

--- internal/tui/poll.go
+++ internal/tui/poll.go
@@ -1,7 +1,11 @@
+// internal/tui/poll.go
 package tui
 
 import (
+	"time"
+
 	tea "github.com/charmbracelet/bubbletea"
+
 	"github.com/dhruvsaxena1998/cleo/internal/cli"
 	"github.com/dhruvsaxena1998/cleo/internal/projects"
 	"github.com/dhruvsaxena1998/cleo/internal/state"
@@ -14,10 +18,26 @@ type stateLoadedMsg struct {
 
 type tickStateMsg struct{}
 
+type paneCapturedMsg struct {
+	sid     string
+	content string
+}
+
 func loadStateCmd(c *cli.Ctx) tea.Cmd {
-	return func() tea.Msg { return stateLoadedMsg{} }
+	return func() tea.Msg {
+		ps, _ := c.Projects.List()
+		ss, _ := c.State.List()
+		return stateLoadedMsg{projects: ps, sessions: ss}
+	}
 }
 
 func tickStateCmd() tea.Cmd {
-	return func() tea.Msg { return tickStateMsg{} }
+	return tea.Tick(750*time.Millisecond, func(time.Time) tea.Msg { return tickStateMsg{} })
+}
+
+func capturePaneCmd(c *cli.Ctx, sid string, lines int) tea.Cmd {
+	return func() tea.Msg {
+		out, _ := c.Tmux.CapturePane(sid, lines)
+		return paneCapturedMsg{sid: sid, content: out}
+	}
 }
